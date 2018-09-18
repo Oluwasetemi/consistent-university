ff971bf874b6a9171ed87b79476c648feb4df3bf
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-09-18 02:33:00 +0100

    build + export

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/admission': { page: '/admission' },
+      '/calender': { page: '/calender' },
+      '/colleges': { page: '/colleges' },
+      '/library': { page: '/library' },
+      '/news': { page: '/news' },
+      '/portal': { page: '/portal' },
+      '/profiles': { page: '/profiles' },
+      '/publication': { page: '/publication' },
+      '/research': { page: '/research' },
+      '/webmail': { page: '/webmail' },
+    }
+  }
+}
