195de3812cb181016e5c8c284a1fbf2c303338d3
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-09-18 04:03:39 +0100

    fixed the about page

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,52 @@ export default () => (
   <div>
     <Header />
     <h1>About</h1>
-    <p>Educational Website created for learning purpose</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
+    <p>An Assignment to ...</p>
     <Footer />
+    <style jsx >
+      {`h1 {
+        font-size:2.2em;
+        padding: 20px;
+        display: grid;
+        grid-template-columns: 1fr auto 1fr;;
+        grid-gap: 30px;
+        align-items: center;
+        margin-bottom: -10px;
+      }
+
+      h1:before, h1:after {
+        display: block;
+        content: '';
+        height: 20px;
+        background: linear-gradient(to var(--direction, left), #351851, transparent );
+      }
+
+      h1:after {
+        --direction: right;
+      }
+
+      p {
+        padding: 20px;
+        background: linear-gradient(to var(--direction, left), var(--yellow, red), transparent );
+        border-radius: 5px;
+        font-size:1.2em;
+        font-weight: 300;
+      }
+
+      p:nth-child(2n) {
+        --yellow: #ffc600;
+        --direction: right;
+      }
+
+     `}
+    </style>
   </div>
 )
